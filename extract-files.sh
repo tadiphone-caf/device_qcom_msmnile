b46ac7fc73e4d693a7cd75b6bc751b7c50fc6a83
Peter Cai <peter@typeblog.net>
2020-09-25 23:16:12 +0300

    msmnile: Make camera blob load with v28 libc++
    * It requires __sfp_handle_exceptions
    
    Change-Id: I3dc6746dd72e9bac69bffe673aefe79849d5d72a

--- extract-files.sh
+++ extract-files.sh
@@ -34,6 +34,14 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib64/hw/camera.qcom.so)
+        sed -i "s/libc++.so/libcXD.so/g" "${2}"
+        ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 SECTION=
